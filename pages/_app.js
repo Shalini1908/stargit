cf393667ad8f248fe1eee056bf0834d7ec26c0f8
Shalini 1908 <989163962g@gmail.com>
2023-01-09 10:59:50 +0530

    acded

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import '../styles/globals.css'
+import  Counter from "./counter"
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return 
+  (
+    <>
+        <Counter />
+    <Component {...pageProps} /></>
+
+  )
+
 }
---
+++ pages/counter.jsx
@@ -0,0 +1,14 @@
+import React , {useState} from 'react'
+
+const Counter = () => {
+    
+  return (
+    <div>
+<h1>Counter</h1>
+
+
+    </div>
+  )
+}
+
+export default Counter
